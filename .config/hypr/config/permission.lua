2f2fca093eff32c81f9498098577e4beb4158298
Wichayut Phongphanpanya <wichayutdew@gmail.com>
2026-07-12 15:16:48 +0700

    move screen capture permisson

---
+++ .config/hypr/config/permission.lua
@@ -0,0 +1,13 @@
+-- https://wiki.hypr.land/Configuring/Advanced-and-Cool/Permissions/
+-----------------------
+----- PERMISSIONS -----
+-----------------------
+-- Please note permission changes here require a Hyprland restart and are not applied on-the-fly
+hl.config({
+	ecosystem = {
+		enforce_permissions = true,
+	},
+})
+
+hl.permission("/usr/(bin|local/bin)/grim", "screencopy", "allow")
+hl.permission("/usr/(lib|libexec|lib64)/xdg-desktop-portal-hyprland", "screencopy", "allow")
--- .config/hypr/hyprland.lua
+++ .config/hypr/hyprland.lua
@@ -3,6 +3,7 @@ require("config/monitor")
 require("config/auto_start")
 require("config/decoration")
 require("config/input")
+require("config/permission")
 
 -- https://wiki.hypr.land/Configuring/Advanced-and-Cool/Environment-variables/
 -------------------------------
@@ -10,20 +11,3 @@ require("config/input")
 -------------------------------
 hl.env("XCURSOR_SIZE", "24")
 hl.env("HYPRCURSOR_SIZE", "24")
-
------------------------
------ PERMISSIONS -----
------------------------
--- See https://wiki.hypr.land/Configuring/Advanced-and-Cool/Permissions/
--- Please note permission changes here require a Hyprland restart and are not applied on-the-fly
--- for security reasons
-
--- hl.config({
---   ecosystem = {
---     enforce_permissions = true,
---   },
--- })
---
--- hl.permission("/usr/(bin|local/bin)/grim", "screencopy", "allow")
--- hl.permission("/usr/(lib|libexec|lib64)/xdg-desktop-portal-hyprland", "screencopy", "allow")
--- hl.permission("/usr/(bin|local/bin)/hyprpm", "plugin", "allow")
