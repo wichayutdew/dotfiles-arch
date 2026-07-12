c4e1b44f9591a2010cd56bf3cd7ace0e64a88b69
Wichayut Phongphanpanya <wichayutdew@gmail.com>
2026-07-12 12:38:43 +0700

    update hyprland conf

--- .config/hypr/config/decoration.lua
+++ .config/hypr/config/decoration.lua
@@ -6,7 +6,7 @@
 hl.config({
 	general = {
 		gaps_in = 5,
-		gaps_out = 10,
+		gaps_out = 5,
 		border_size = 3,
 		col = {
 			active_border = { colors = { "rgba(33ccffee)", "rgba(00ff99ee)" }, angle = 45 },
